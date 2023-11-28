8b2ee28f32319010c677053cb77bd7e21543f77d
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2023-11-28 06:52:28 +0100

    docker install

--- install.sh
+++ install.sh
@@ -27,8 +27,29 @@ echo ""
 # Install specifics
 # 
 if [[ $osv == "Ubuntu_22.04" ]]; then
+    
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    sudo usermod -aG docker $USER
+    
     wget -P /usr/lib/udev/rules.d \
         ${ASSETS_URL}/85-brltty.rules
+    
 fi
 
 # ---
